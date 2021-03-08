dec9a73240ef9aee346a320ebbfc88efc55ba0f5
muharremkc58 <kmuho5885@gmail.com>
2021-03-08 16:20:01 +0300

    First Commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,29 +14,40 @@ public class DemoApplication {
     public static void main(String[] args) throws IOException {
 
         SpringApplication.run(DemoApplication.class, args);
-
+        Scanner keyboard = new Scanner(System.in);
+        keyboard.useLocale(Locale.US);
          int number,choose;
-         double personNumber;
+         float length;
          char character;
-         String words;
+         String word;
          boolean must;
          number=1922;
-         personNumber=1.70;
+        length=1.70f;
          character='M';
-         words="Muharrem";
+         word="Muharrem";
          must=true;
          System.out.println("If you want print select 1,but if you want write select 2");
          System.out.println("Please your choose:");
-         Scanner keyboard = new Scanner(System.in);
          choose = keyboard.nextInt();
          if(choose==1) {
              System.out.println(number);
-             System.out.println(personNumber);
+             System.out.println(length);
              System.out.println(character);
-             System.out.println(words);
+             System.out.println(word);
              System.out.println(must);
          }
+         if(choose==2){
+             System.out.print("Number:");
+             number= keyboard.nextInt();
+             System.out.print("Length:");
+             length=keyboard.nextFloat();
+             System.out.print("Character:");
+             character= keyboard.next().charAt(0);
+             System.out.print("Word:");
+             word= keyboard.next();
+               keyboard.close();
 
+         }
         }
     }
 
